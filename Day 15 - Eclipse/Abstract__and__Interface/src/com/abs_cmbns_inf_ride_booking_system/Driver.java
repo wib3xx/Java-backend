0fabb046e05972cedbe6e87c8f1b16dc21ee399f
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-07-24 23:49:02 +0530

    Update

--- Day 15 - Eclipse/Abstract__and__Interface/src/com/abs_cmbns_inf_ride_booking_system/Driver.java
+++ Day 15 - Eclipse/Abstract__and__Interface/src/com/abs_cmbns_inf_ride_booking_system/Driver.java
@@ -3,11 +3,11 @@ package com.abs_cmbns_inf_ride_booking_system;
 public class Driver {
 
 	public static void main(String[] args) {
-		System.out.println("======= Rapido Booking Status =======");
+		System.out.println("========= Rapido Booking Status =========");
 		IPayment payment;
 		Ride ride;
 		
-		System.out.println("------------ UPI Ride ------------");
+		System.out.println("------------ Ride using UPI ------------");
 		payment = new UPIRide();
 		ride = new UPIRide();
 		
