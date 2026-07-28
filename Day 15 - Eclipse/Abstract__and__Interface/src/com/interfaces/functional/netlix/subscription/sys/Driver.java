6c963191c49047bb6e5157ef0077974eadd5625b
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-07-28 23:52:47 +0530

    Netflix Membership System

---
+++ Day 15 - Eclipse/Abstract__and__Interface/src/com/interfaces/functional/netlix/subscription/sys/Driver.java
@@ -0,0 +1,32 @@
+package com.interfaces.functional.netlix.subscription.sys;
+
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		System.out.println("-------- Netflix India ------");
+		System.out.println();
+		
+		System.out.println("-------  Plan Status ------");
+		Predicate<Integer> memberStatus = (plan) -> plan > 1300;
+		System.out.println("Premium User?: "+memberStatus.test(800));
+		
+		Supplier<Integer> subscriptionId = () -> 100000 + new Random().nextInt(900000);
+		System.out.println("Subscription ID: "+subscriptionId.get());
+		
+		Consumer<String> email = (emailTo) -> System.out.println("Email sent to: "+emailTo);
+		email.accept("sarvagay123gist@gmail.com");
+		
+		Function<Integer, Integer> couponAmt = (amt) -> amt = amt - (amt*20/100);
+		System.out.println("Plan: Premium");
+		System.out.println("Price of Membership: Rs. 80000");
+		System.out.println("Coupon Worth: 20% Discount");
+		System.out.println("Price after Coupon: Rs."+ couponAmt.apply(80000));
+		System.out.println("Subscription Added");
+	}
+}
---
+++ Day 15 - Eclipse/Abstract__and__Interface/src/com/interfaces/functional/netlix/subscription/sys/NetflixMembership.java
@@ -0,0 +1,6 @@
+package com.interfaces.functional.netlix.subscription.sys;
+
+public interface NetflixMembership {
+
+	abstract int amount(int price);
+}
