63b3d3421612c316dea73b68947796df6d5dcf1e
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-07-28 22:24:11 +0530

    Marks Calc Sys

---
+++ Day 15 - Eclipse/Abstract__and__Interface/src/com/interfaces/functional/clg/student/result/sys/Driver.java
@@ -0,0 +1,27 @@
+package com.interfaces.functional.clg.student.result.sys;
+
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		Predicate<Integer> eligibility = (marks) -> marks >= 40;
+		System.out.println("Marks = 44");
+		System.out.println("Passing Marks cleared = "+eligibility.test(44));
+		System.out.println("Result: Pass");
+		
+		Supplier<Integer> rollno = () -> 100000 + new Random().nextInt(900000);
+		System.out.println("Roll Number : "+ rollno.get());
+		
+		Consumer<String> sms = (smsTo) -> System.out.println("SMS sent to: "+ smsTo);
+		sms.accept("7764534410");
+		
+		Function<Integer, Integer > graceMarks = (grace) -> grace = grace+ 5;
+		System.out.println("Grace Marks: "+ graceMarks.apply(44));
+	}
+}
---
+++ Day 15 - Eclipse/Abstract__and__Interface/src/com/interfaces/functional/clg/student/result/sys/ResultCalculator.java
@@ -0,0 +1,6 @@
+package com.interfaces.functional.clg.student.result.sys;
+
+public interface ResultCalculator {
+
+	abstract  int calculateMarks(int marks);
+}
