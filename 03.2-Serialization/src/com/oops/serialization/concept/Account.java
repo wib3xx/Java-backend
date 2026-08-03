374c65363b00235b76d5598f412c2472ac1936ea
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-08-03 22:35:33 +0530

    Update

--- 03.2-Serialization/src/com/oops/serialization/concept/Account.java
+++ 03.2-Serialization/src/com/oops/serialization/concept/Account.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class Account implements Serializable{
 
 	
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;  
 	private transient int balance;          //transient --> skips the serialization
 	private String name;
 	private String password;
--- 03.3-Cloning/src/com/clonings/concept/Driver.java
+++ 03.3-Cloning/src/com/clonings/concept/Driver.java
@@ -28,7 +28,7 @@ class Employee{
 public class Driver {
 
 	public static void main(String[] args) throws CloneNotSupportedException {
-		Employee e1 = new Employee("Sarvagay", new Address("Banglore, KA, IND"));
+		Employee e1 = new Employee("Sarvagay_Sanmotra", new Address("Banglore, KA, IND"));
 		
 		Employee e2 =(Employee) e1.clone();
 		System.out.println("Name: "+e2.name);
