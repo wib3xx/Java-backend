19c355d91110bc25792d040c85611a0c164ef3e6
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-08-06 19:18:12 +0530

    Exception Handling

--- 04-ExceptionHandling/src/com/zepto/Driver.java
+++ 04-ExceptionHandling/src/com/zepto/Driver.java
@@ -2,6 +2,12 @@ package com.zepto;
 
 public class Driver {
 
-	
+	public static void main(String[] args) {
+		
+		System.out.println("Extecution STARTS");
+		String name = "Sarvagay Sanmotra";
+		System.out.println("Length of String is: "+ name.length());
+		System.out.println("Execution ENDS");
+	}
 	
 }
