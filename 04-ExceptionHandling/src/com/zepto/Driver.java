4f6b8a5a4373d5daa0472d2fcfc0968325a588e5
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-08-07 18:41:35 +0530

    update

--- 04-ExceptionHandling/src/com/zepto/Driver.java
+++ 04-ExceptionHandling/src/com/zepto/Driver.java
@@ -6,9 +6,18 @@ public class Driver {
 		
 		System.out.println("Extecution STARTS");
 		//String name = "Sarvagay Sanmotra";
-		String name = null;    //Null Pointer exception
+		String name = null;    //Null Pointer exception -->null.length()
+		
 		System.out.println("Length of String is: "+ name.length());
+		Driver driver = new Driver();
+		driver.doSomething();
 		System.out.println("Execution ENDS");
+	}
+	
+	private void doSomething() {
+		System.out.println("START");
+		System.out.println("Doing something");
+		System.out.println("END");
 	}
 	
 }
