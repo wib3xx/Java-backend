9adad00807914b2d5e8b12dd937b0298d4e72789
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-08-10 19:00:08 +0530

    Update- Exception Handling

---
+++ 04-ExceptionHandling/src/com/zepto/NPEPracticeDriver.java
@@ -0,0 +1,27 @@
+package com.zepto;
+
+public class NPEPracticeDriver {
+
+	public static void main(String[] args) {
+		System.out.println("Execution STARTS");
+		try {
+		String name = null;
+		System.out.println("Name length: "+ name.length());
+		
+		NPEPracticeDriver pdriver = new NPEPracticeDriver();
+		pdriver.doSomething();
+		
+		}
+		catch(NullPointerException e) {
+			System.out.println("Name is not Mentioned!");
+			e.printStackTrace();
+		}
+		System.out.println("Execution ENDS");
+	}
+	
+	private void doSomething() {
+		System.out.println("WORK START");
+		System.out.println("Doing Something");
+		System.out.println("WORK DONE");
+	}
+}
