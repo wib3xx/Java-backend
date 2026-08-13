c6d8823fd545f289c135b2b38e70d2ae4bff8e2b
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-08-13 19:40:24 +0530

    Update: Practice for ArrayList

---
+++ 05-Collection_Framework/src/com/collections/practice1/CountryNames.java
@@ -0,0 +1,26 @@
+package com.collections.practice1;
+
+import java.util.ArrayList;
+
+public class CountryNames {
+
+	public ArrayList<String> getCountryName(){
+		ArrayList<String> country = new ArrayList();
+		country.add("Australia");
+		country.add("Ausria");
+		country.add("Algeria");
+		country.add("Africa");
+		country.add("Argentina");
+		country.add("Iran");
+		country.add("India");
+		country.add("China");
+		country.add("Portuguse");
+		country.add("Spain");
+		country.add("Russia");
+		country.add("UK");
+		country.add("USA");
+		country.add("Brazil");
+		
+		return country;
+	}
+}
---
+++ 05-Collection_Framework/src/com/collections/practice1/Driver.java
@@ -0,0 +1,20 @@
+package com.collections.practice1;
+
+import java.util.ArrayList;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		CountryNames countryNames = new CountryNames();
+		
+		//Getting country names
+		ArrayList<String> countries = countryNames.getCountryName();
+		
+		for(String country : countries) {
+			if(country.startsWith("A")) {
+				System.out.println(country);
+			}
+		}
+		
+	}
+}
